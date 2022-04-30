1b1b26606cd75274d25c5cc7d0a155821441345f
Seth <sethduffin@gmail.com>
2022-04-30 16:03:16 -0600

    Updated unit 09

--- unit/09/js/main.js
+++ unit/09/js/main.js
@@ -1,5 +1,18 @@
-import { updatePageText } from './pageText.js';
+function duplicateMenu() {
+  const topList = document.querySelectorAll('ul#primaryNavigation li a');
+  const newList = document.createElement('ul');
 
-window.addEventListener('load', () => {
-  updatePageText();
-});
+  topList.forEach((menuItem) => {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.setAttribute('href', menuItem.getAttribute('href'));
+    link.innerText = menuItem.innerText;
+    listItem.appendChild(link);
+
+    newList.appendChild(listItem);
+  })
+
+  document.querySelector('#smallNavArea').appendChild(newList);
+}
+
+duplicateMenu();
